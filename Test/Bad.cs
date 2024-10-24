9472eaf430ac0007447b499127b1c6d2eb23ec39
Insaf BELHAFI <ibelhafi.ext@lebonmarche.fr>
2024-10-24 14:33:09 +0200

    add classe Bad

---
+++ Test/Bad.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Bad
+    {
+        void M()
+        {
+            Logger.Log("Hello, World!");
+        }
+
+        static class Logger
+        {
+            [Obsolete("Use Log(LogLevel level, string s) instead")]
+            public static void Log(string s)
+            {
+                // ...
+            }
+
+            public static void Log(LogLevel level, string s)
+            {
+                // ...
+            }
+        }
+
+        enum LogLevel
+        {
+            Info,
+            Warning,
+            Error
+        }
+    }
+}
